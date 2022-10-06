e02b2dc1fe2732fff5fb8f15dee689f989435050
Banaanae <83927639+Banaanae@users.noreply.github.com>
2022-10-06 08:03:39 +0000

    Create StackCalc.cs

---
+++ StackCalc.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace StackCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter item amount: ");
+                double itemAmount = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter max stack: ");
+                double maxStack = Convert.ToDouble(Console.ReadLine());
+                double stackAmount = Math.Floor(itemAmount / maxStack);
+                double stackRemainder = itemAmount % maxStack;
+                double shulkerBoxAmount = Math.Ceiling(stackAmount / 27);
+                double doubleChestAmount = Math.Ceiling(stackAmount / 54);
+                Console.WriteLine($"{itemAmount} is equal to: {stackAmount} stacks and {stackRemainder} items.");
+                Console.WriteLine($"You would need {shulkerBoxAmount} shulker boxes or {doubleChestAmount} double chests to store all the items.\n");
+            }
+        }
+    }
+}
